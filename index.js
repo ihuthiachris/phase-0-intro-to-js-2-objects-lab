db7671d11fdafe0173ce175a72c50b431386cfb3
Chris Ihuthia Kinuthia <ihuthiachris@gmail.com>
2022-08-24 08:32:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Chris',
+    streetAddress: 'Kiambu'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+   const employeeCopy = employee;
+   employeeCopy.name = 'Sam'
+
+   const updatedEmployee = {...employeeCopy, streetAddress: '11 Broadway'};
+   return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee.name = 'Sam'
+    employee.streetAddress = '12 Broadway'
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const employeeCopy = {...employee, name: undefined};
+    return employeeCopy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name;
+    return employee;
+}
